2307cc698624c875e13e3ed8a45395a1e649f19e
Pizzo <felipepizzorno98@gmail.com>
2023-07-31 03:30:46 -0300

    Working colorpicker for wheel

--- engine.js
+++ engine.js
@@ -1,9 +1,26 @@
 
+    const cycle = num => num % pixelAmount
+    function hexToRgb(hex) {
+      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+      });
+
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+      ] : null;
+    }
     // Seconds since epoch
     const getTime = () => Math.round(Date.now())
     const updatePixels = (pixelIds, rgb, newPixelColors) => {
         pixelIds.forEach(pixelId => newPixelColors[pixelId] = rgb)
     }
+    const pixelList = [0, 1, 2, 3, 4, 5, 6, 7]
+    const resetPixels = (newPixelColors) => updatePixels(pixelList, [0, 0, 0], newPixelColors)
     const newFrame = (pixelIds, rgb, defaultRgb) => {
         let newPixelColors = []
         for(let i = 0; i < pixelAmount; i++) {
@@ -42,9 +59,12 @@
     // Get the canvas element from the DOM
     let c = document.getElementById('exCanvas')
     let ctx = c.getContext('2d')  
-    const sumVectors = ([c11, c12], [c21,c22]) => [c11 + c21, c12 + c22]
+    const sum3Vectors = ([c11, c12, c13], [c21,c22, c23]) => [c11 + c21, c12 + c22, c13 + c23]
+    const scale3Vector = ([c1, c2, c3], scalar) => [scalar * c1, scalar * c2, scalar * c3]
+    const substract3Vectors = ([c11, c12, c13], [c21, c22, c23]) => [c11 - c21, c12 - c22, c13 - c23]
+    const sumVectors = ([c11, c12], [c21, c22]) => [c11 + c21, c12 + c22]
     const scaleVector = (scalar, [c1, c2]) => [scalar * c1, scalar * c2]
-    const substractVectors = ([c11, c12], [c21,c22]) => [c11 - c21, c12 - c22]
+    const substractVectors = ([c11, c12], [c21, c22]) => [c11 - c21, c12 - c22]
 
     const rotate = (angle, baseVector, tangentVector) => {
       let cosBase = scaleVector(Math.cos(angle), baseVector)
@@ -73,7 +93,7 @@
             ctx.beginPath()
             ctx.moveTo(x, y)
             ctx.lineTo(x + fanPoints[i][0], y + fanPoints[i][1])
-            ctx.lineTo(x + fanPoints[(i + 1) % pixelAmount][0], y + fanPoints[(i + 1) % pixelAmount][1])
+            ctx.lineTo(x + fanPoints[cycle(i + 1)][0], y + fanPoints[cycle(i + 1)][1])
             ctx.fillStyle = `rgb(${pixelsColors[i][0]}, ${pixelsColors[i][1]}, ${pixelsColors[i][2]})` 
             ctx.fill()
             ctx.closePath()
@@ -104,19 +124,19 @@
     updatePixels([0, 1, 2, 3, 4, 5, 6, 7], [0, 0, 0], defaultBaseFrame)
     const defaultUpdate = (animationState) => animationState
 
-    function initEngine(calculateNewAnimationState = defaultUpdate, baseFrame = defaultBaseFrame, nextFrameThreshold = 1000, interpolationStart = 0) {
+    function initEngine(calculateNewAnimationState = defaultUpdate, initialState = {}, baseFrame = defaultBaseFrame, nextFrameThreshold = 500, interpolationStart = 0) {
       let animationState = {
           oldFrame: [...baseFrame],
           nextFrame: [...baseFrame],
           lastFrameTime: getTime(),
           timeSinceFrame: 0,
-          extraFields: {},
+          extraFields: initialState,
       }
 
       const update = () => {
           let currentTime = getTime()
           timeSinceFrame = currentTime - animationState.lastFrameTime
-          let actualNextFrameThreshold = 500 / speed
+          let actualNextFrameThreshold = nextFrameThreshold / speed
           let frame = []
           let shouldStop = speed === 0
           let shouldCalculateFrame = timeSinceFrame >= actualNextFrameThreshold
